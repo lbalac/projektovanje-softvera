bf3d1cf7d07304c6e49106af8a111f07ab8f1681
luka balac <balac.luka12@gmail.com>
2026-01-07 19:51:12 +0100

    prvi commit nema nista

---
+++ src/ps_seminarski_lukabalac20230082/Ps_seminarski_lukabalac20230082.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ps_seminarski_lukabalac20230082;
+
+/**
+ *
+ * @author balac
+ */
+public class Ps_seminarski_lukabalac20230082 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //od negde se mora poceti :)
+    }
+    
+}
